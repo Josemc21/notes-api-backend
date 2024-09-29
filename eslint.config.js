91fb39d75a98d5c1654b206e7139250d791524d1
Jose Antonio Marañón Calvario <jose.maranoncalvario@alum.uca.es>
2024-09-29 20:34:53 +0200

    Testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,23 @@
-const { globals, commonjs} = require('globals')
-const pluginJs = require('@eslint/js')
+const globals = require('globals') // Asegúrate de que esto funcione
+const pluginJs = require('@eslint/js') // Importa el plugin ESLint JS
 
-export default [
+module.exports = [ // Cambia export default a module.exports
   {
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2021,   // O "12", si prefieres, para ECMAScript 12 (2021)
-      sourceType: 'module',
+      sourceType: 'commonjs', // Cambia a 'commonjs'
       globals: {
         ...globals.node,    // Soporte para entorno Node.js
-        ...globals.browser  // Soporte para entorno del navegador
+        ...globals.browser   // Soporte para entorno del navegador
       },
     },
     rules: {
-      indent: ['error', 2],               // Indentación de 2 espacios
-      quotes: ['error', 'single'],        // Usar comillas simples
-      semi: ['error', 'never'],           // No requiere punto y coma
-      'linebreak-style': ['error', 'off'], // Desactiva la regla
-    },
-    env : {
-      browser: true,
-      node: true,
-      es2021: true,
-      commonjs: true
-    },
+      indent: ['error', 2],                // Indentación de 2 espacios
+      quotes: ['error', 'single'],         // Usar comillas simples
+      semi: ['error', 'never'],            // No requiere punto y coma
+      'no-unused-vars': ['warn'],          // Avisa sobre variables no usadas
+    },    
   },
   // Añadir las configuraciones recomendadas de ESLint para JS
   pluginJs.configs.recommended
