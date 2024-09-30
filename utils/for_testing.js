9a7df0bf012f2ef4ae1f0a41688e022e464e7d08
Jose Antonio Marañón Calvario <jose.maranoncalvario@alum.uca.es>
2024-09-30 21:46:10 +0200

    Testing

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -1,8 +1,13 @@
 const palindrome = (string) => {
+  if (typeof string === 'undefined') return undefined
+
   return string.split('').reverse().join('')
 }
 
 const average = (array) => {
+  if (typeof array === 'undefined') return 0
+  if (array.length === 0) return 0
+
   let sum = 0
   array.forEach(num => sum += num)
   return sum / array.length
